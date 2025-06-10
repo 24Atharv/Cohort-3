99395cd0b70934f7be9411fcddaa6e4bc2a09355
Atharv Nandrukar <atharvnandurkar@gmail.com>
2025-06-10 23:12:02 +0000

    jsonwebtoken added

--- week-6-Auth/index.js
+++ week-6-Auth/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
-
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = 'randomatharvzzxatg'
 
 const users = [];
 
