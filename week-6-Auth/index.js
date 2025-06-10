a2a0b3d348cb856be42513afb66012dfa0e8e768
Atharv Nandrukar <atharvnandurkar@gmail.com>
2025-06-10 15:56:08 +0000

    Signin route created

--- week-6-Auth/index.js
+++ week-6-Auth/index.js
@@ -18,10 +18,30 @@ app.post('/signup', (req, res) => {
     message: "Signup is Done"
    });
 
+   console.log(users);
+
 })
 
 app.post('/signin', (req, res) => {
+   const username = req.body.username
+   const password = req.body.password
 
+   let foundUser = null;
+   for(let i=0; i < users.length; i++) {
+      if(users[i].username === username && users[i].password === password) {
+         foundUser = users[i];
+      } 
+   }
+      if(!foundUser) {
+         res.json({
+            message: "Invalid"
+         });
+   } else {
+      res.json({
+         message: "Signin succesfull"
+      })
+   }
+   console.log(users);
 })
 
 app.listen(4000, () => console.log("Server Started"));
