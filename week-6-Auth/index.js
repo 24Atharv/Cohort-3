48457705ad6d1042bdd11babe4ff0ed5d2eaa9ba
Atharv Nandrukar <atharvnandurkar@gmail.com>
2025-06-10 10:26:49 +0000

    route created

---
+++ week-6-Auth/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/signup', (req, res) => {
+    
+})
+
+app.get('/signin', (req, res) => {
+
+})
+
+app.listen(4000, () => console.log("Server Started"));
