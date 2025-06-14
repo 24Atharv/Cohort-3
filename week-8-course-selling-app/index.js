1c214ddda3dae7783da03af81b1730ab287d5266
Atharv Nandrukar <atharvnandurkar@gmail.com>
2025-06-14 10:51:10 +0000

    express server added

---
+++ week-8-course-selling-app/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const { mongoose } = require('mongoose');
+const app = express();
+app.use(express.json());
+
+function main() {
+    mongoose.connect('mongodb+srv://atharvwork:1224@cluster0.xsevoja.mongodb.net/coursera');
+    app.listen(3000);
+    console.log("Server Started");
+}
+main();
+
+app.route();
+
