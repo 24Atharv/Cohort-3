88514a6ffd28a064053bb7484cb8f2e730b1fb9b
Atharv Nandrukar <atharvnandurkar@gmail.com>
2025-06-15 16:07:23 +0000

    admin Route endpoint added

--- week-8-course-selling-app/routes/admin.js
+++ week-8-course-selling-app/routes/admin.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const adminRoute = express.Router();
 const { adminMiddleware } = require('../middlewares/admin');
-const { adminModel } = require('../db');
+const { adminModel, courseModel } = require('../db');
 const jwt = require('jsonwebtoken');
 const { JWT_ADMIN_PASSWORD } = require('../config');
 
@@ -44,6 +44,52 @@ adminRoute.post('/signin', async (req, res) => {
             message: "Incorrect credentials"
         });
     }
+})
+
+adminRoute.post('/course', adminMiddleware, async (req, res) => {
+    const adminId = req.userId
+
+    const course = await courseModel.create({
+        title, price, imageUrl, creatorId: adminId
+    }) 
+
+    res.json({
+        courseId: course._id,
+        message: "course created"
+    }) 
+})
+
+adminRoute.put('/course', adminMiddleware, async (req, res) => {
+    const adminId = req.userId
+
+    const course = await courseModel.updateOne({
+        _id: courseId,
+        cratorId: adminId   }, 
+        {
+            title,
+            description,
+            price,
+            imageUrl
+    }); 
+
+    res.json({
+        courseId: course._id,
+        message: "course updated"
+    })
+
+})
+
+adminRoute.get('/courseBulk', (req, res) => {
+    const adminId = req.userId;
+
+    const course = courseModel.find({
+        creatorId: adminId._id
+    })
+
+    res.json({
+        course,
+        message: "get course"
+    })
 })
 
 module.exports = {
